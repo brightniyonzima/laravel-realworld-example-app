1f6a3961ca81480d26ba871c383dfe127864c24e
bright niyonzima <brightniyonzima@gmail.com>
2019-04-09 16:21:44 +0300

    setup local environment configurations

--- config/jwt.php
+++ config/jwt.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_SECRET', 'BvjpTAv86dMLdZdrGRSW6GFY9kbJXI3R'),
+    'secret' => env('JWT_SECRET', 'LwuKznxrHyot5Xdxk1ZV0MnbGarKR5xI'),
 
     /*
     |--------------------------------------------------------------------------
